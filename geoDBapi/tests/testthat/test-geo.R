e385d0075dccc4eb84ccefe63f7ee993a0a2efb8
Andre Gnandt <gnandta@student.ubc.ca>
2026-02-01 21:32:53 -0800

    example test

--- geoDBapi/tests/testthat/test-geo.R
+++ geoDBapi/tests/testthat/test-geo.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("get countries", {
+  geo <- GEO$new()
+  countries <- geo$FindCountries()
+  expect_gt(countries$count, 0)
 })
